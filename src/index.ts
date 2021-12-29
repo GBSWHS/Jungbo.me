9e5aae7ecad4a304df17482c515752430dcd24b9
noeulnight <noeulnight.mail@gmail.com>
2021-12-29 16:35:13 +0900

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv'
+import problem from './utils/problem'
+import { Client, Intents, MessageEmbed } from 'discord.js'
+
+dotenv.config()
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+bot.on('ready', () => console.log('Ready!'))
+
+bot.on('message', async (input) => {
+  if (input.author.bot) return
+  if (!input.content.startsWith('-')) return
+  const msg = input.content.slice(1)
+
+  if (msg.startsWith('GET')) {
+    const id = msg.split(' ')[1] as string
+    if (!id) {
+      input.reply('아이디를 입력해주세요.')
+      return
+    }
+    const info = await problem(id)
+
+    if (!info.success) {
+      input.channel.send('문제를 찾을 수 없습니다!')
+      return
+    }
+    const embed = new MessageEmbed({ color: 'RED' })
+    const embed2 = new MessageEmbed({ color: 'RED' })
+    input.channel.send({
+      embeds: [
+        embed.setTitle(id + ' 문제를 불러온 결과입니다.')
+          .addField('문제 설명', '```' + info.head + '```')
+          .addField('입력 설명', '```' + info.idesc + '```')
+          .addField('출력 설명', '```' + info.odesc + '```'),
+        embed2.setTitle('입출력').addField('입력', '```' + info.input + '```', true).addField('출력', '```' + info.output + '```', true)
+      ]
+    })
+  }
+})
+
+bot.login(process.env.TOKEN)
---
+++ src/utils/problem.ts
@@ -0,0 +1,37 @@
+import { get } from 'superagent'
+import cheerio from 'cheerio'
+
+const problem = async (id: string) => {
+  try {
+    const url = (await problems()).filter(p => p.id === id.trim())[0].src
+    const { text } = await get(`http://ot.jungbo.me/${url}/`)
+    const $ = cheerio.load(text)
+
+    const head = $('div.panel:nth-child(1) > div:nth-child(2)').text().trim()
+    const idesc = $('.container > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2)').text().trim()
+    const odesc = $('div.panel:nth-child(3) > div:nth-child(2)').text().trim()
+    const input = $('#sampleinput').text().trim()
+    const output = $('#sampleoutput').text().trim()
+
+    return { success: true, head, idesc, odesc, input, output }
+  } catch {
+    return { success: false }
+  }
+}
+
+const problems = async () => {
+  const { text } = await get('http://ot.jungbo.me/contest.php?cid=1018')
+  const $ = cheerio.load(text)
+
+  const data = [] as { id: string, src: string }[]
+  const table = $('tbody>tr')
+  table.each((i, el) => {
+    const td = $(el).find('td')
+    const id = td.eq(1).text().trim()
+    const src = td.eq(1).find('a').attr('href') || ''
+    data.push({ id, src })
+  })
+  return data
+}
+
+export default problem
